c4604c341bbd2daa3095f35995a9fa3b9633b35b
cfutrell09 <cfutrell09@gmail.com>
2023-05-31 15:18:51 -0700

    Updated Pokemon List Array
    Last one was wrong, here is updated!

--- charles-portfolio-app/js/scripts.js
+++ charles-portfolio-app/js/scripts.js
@@ -1,1 +1,25 @@
-Pokemonlist= [{name: "balbasaure", type: "grass"}{name: "charmander", type: "fire"}{name: "pikachu", type: "lightning"}{name: "squirtle", type: "water"}]
+let pokemonList= [
+    {
+    name: "balbasaure", 
+    type: "grass", "poison"
+    height: 7
+    }
+
+    {
+        name: "charmander", 
+        type: "fire",
+        height: 6
+    }
+    
+    {
+        name: "pikachu", 
+        type: "lightning",
+        height: 4
+    }
+    
+    {
+        name: "squirtle", 
+        type: "water",
+        height: 5
+    }
+]
